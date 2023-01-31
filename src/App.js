882e0cf15434145c645ac525663e783cd5d227c0
dayo <dahurdayo@example.com>
2023-01-31 15:26:35 +0100

    tailwind practice

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css'
+import Layout from "./components/Layout";
+
+function App(){
+    return (
+        <div className="relative container mx-auto p-6">
+            <Layout/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App'
+
+ReactDom.render(
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+    , document.getElementById('root')
+)
